fb0326b77629f826a7f3f9b0cd309a565f59f15d
Moeen Heydari <moeentk@gmail.com>
2024-04-23 09:58:43 +0200

    230424: Goodboy-Oldboy (A dog age calculator) added to the project

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -3,3 +3,24 @@ const yearOfBirth = 1990;
 const yearFuture = 2030;
 const age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture);
+
+// Goodboy-Oldboy (A dog age calculator)
+const dogYearOfBirth = 2017;
+const dogYearFuture = 2030;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let typeOfYears = "human"; // Default type of years set to "human"
+
+// Determine whether to show the result in dog years or human years
+const shouldShowResultInDogYears = true; //change this to false if you want to see results in human years
+if (shouldShowResultInDogYears) {
+    dogYear *= 7;
+    typeOfYears = "dog";
+}
+console.log(
+    "Your dog will be " +
+        dogYear +
+        " " +
+        typeOfYears +
+        " years old in " +
+        dogYearFuture
+);
