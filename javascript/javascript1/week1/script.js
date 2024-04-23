1dd3d84a00606da2708fb46d3680a84fe2ff07d0
Moeen Heydari <moeentk@gmail.com>
2024-04-23 09:57:04 +0200

    230424: Added Age-ify (A future age calculator) to the project

---
+++ javascript/javascript1/week1/script.js
@@ -0,0 +1,5 @@
+// Age-ify (A future age calculator)
+const yearOfBirth = 1990;
+const yearFuture = 2030;
+const age = yearFuture - yearOfBirth;
+console.log("You will be " + age + " years old in " + yearFuture);
