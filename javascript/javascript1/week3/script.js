d96801ed05561c58acc659561bacf781b909fcef
Moeen Heydari <moeentk@gmail.com>
2024-05-05 23:51:36 +0200

    050524: When will we be there?? added to project

--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -22,3 +22,33 @@ console.log(names); // ['Peter', 'Yana', 'Kristina', 'Rasmus', 'Samuel', 'Katrin
 console.log(
     "-----------------------------------------------------------------"
 );
+
+// When will we be there??
+
+const travelInformation = {
+    speed: 50, // km/h
+    destinationDistance: 432, //km
+};
+
+function calculateTravelTime(travelInformation) {
+    // To get speed and destinationDistance from travelInformation
+    const { speed, destinationDistance } = travelInformation;
+
+    // Calculate total time in hours
+    const totalTimeHours = destinationDistance / speed;
+
+    // Calculate hours and minutes separately
+    const hours = Math.floor(totalTimeHours);
+    const minutes = Math.round((totalTimeHours - hours) * 60);
+
+    const hoursAndMinutes = `${hours} hours and ${minutes} minutes`;
+
+    return hoursAndMinutes;
+}
+
+const travelTime = calculateTravelTime(travelInformation);
+console.log(travelTime); // 8 hours and 38 minutes
+
+console.log(
+    "-----------------------------------------------------------------"
+);
