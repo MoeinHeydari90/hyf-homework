3ac5831351d765fb7b69cc3fb33a573c487d9b86
Moeen Heydari <moeentk@gmail.com>
2024-05-05 20:51:10 +0200

    050524: Item array removal added to project

--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -1,1 +1,24 @@
+// Item array removal
 
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "Kristina",
+    "Rasmus",
+    "Samuel",
+    "Katrine",
+    "Tala",
+];
+const nameToRemove = "Ahmad";
+
+// Find the index of the name that should be removed
+const indexToRemove = names.indexOf(nameToRemove);
+// Remove one item at indexToRemove from the names array
+names.splice(indexToRemove, 1);
+
+console.log(names); // ['Peter', 'Yana', 'Kristina', 'Rasmus', 'Samuel', 'Katrine', 'Tala']
+
+console.log(
+    "-----------------------------------------------------------------"
+);
