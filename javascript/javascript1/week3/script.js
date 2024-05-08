3f19af8e61cdd1751adb8828667bbfaa29ccab5c
Moeen Heydari <moeentk@gmail.com>
2024-05-08 09:27:52 +0200

    080524: Some changes applied to the note taking app

--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -112,7 +112,7 @@ function saveNote(content, id) {
     // To avoid adding empty content
     if (content) {
         // Create a new note object with content and id
-        const note = { content: content, id: id };
+        const note = { content, id };
         notes.push(note);
     }
 }
@@ -123,7 +123,11 @@ saveNote("Do laundry", 2);
 console.log(notes); // [{content: 'Pick up groceries', id: 1}, {content: 'Do laundry', id: 2}]
 
 function getNote(id) {
-    return notes[id - 1];
+    for (const note of notes) {
+        if (note.id === id) {
+            return note;
+        }
+    }
 }
 
 const firstNote = getNote(1);
