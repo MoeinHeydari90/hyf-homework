01f45ff5bfbe999abc9addcd53e4a1ac70c787fb
Moeen Heydari <moeentk@gmail.com>
2024-07-10 09:23:41 +0200

    100724: add a new task

---
+++ databases/week2/week2-homework.sql
@@ -0,0 +1,7 @@
+-- Part 1: Working with tasks
+
+
+-- Add a task with these attributes: title, description, created, updated, due_date, status_id, user_id
+INSERT INTO task (title, description, created, updated, due_date, status_id, user_id) values 
+('Learn about TypeScript', 'Study the basics and advanced features of TypeScript for better development practices', '2024-07-10 14:35:00', '2024-07-11 10:00:00', '2024-07-12 18:00:00', 3, 1);
+
