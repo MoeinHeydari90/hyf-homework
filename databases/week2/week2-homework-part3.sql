df1b4fa5b26a48eca176b1b672cec82187d4b607
Moeen Heydari <moeentk@gmail.com>
2024-07-11 00:51:44 +0200

    110724: Get all the tasks for 'Donald Duck' with status 'Not started'

--- databases/week2/week2-homework-part3.sql
+++ databases/week2/week2-homework-part3.sql
@@ -8,3 +8,12 @@ JOIN user_task ON user.id = user_task.user_id
 JOIN task ON user_task.task_id = task.id
 JOIN status ON task.status_id = status.id
 WHERE user.email LIKE '%@spotify.com';
+
+
+-- Get all the tasks for 'Donald Duck' with status 'Not started'
+SELECT task.id, task.title, status.name AS status_name
+FROM user
+JOIN user_task ON user.id = user_task.user_id
+JOIN task ON user_task.task_id = task.id
+JOIN status ON task.status_id = status.id
+WHERE user.name = 'Donald Duck' AND status.name = 'Not started';
