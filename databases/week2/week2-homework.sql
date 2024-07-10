ff42cc39c10e5baddbd8611b227913169f52cfa4
Moeen Heydari <moeentk@gmail.com>
2024-07-10 09:29:47 +0200

    100724: change the title of the last task

--- databases/week2/week2-homework.sql
+++ databases/week2/week2-homework.sql
@@ -5,3 +5,8 @@
 INSERT INTO task (title, description, created, updated, due_date, status_id, user_id) values 
 ('Learn about TypeScript', 'Study the basics and advanced features of TypeScript for better development practices', '2024-07-10 14:35:00', '2024-07-11 10:00:00', '2024-07-12 18:00:00', 3, 1);
 
+
+-- Change the title of a task
+UPDATE task
+SET title = 'Learn about TypeScript and NodeJS'
+WHERE task.id = LAST_INSERT_ID ();
