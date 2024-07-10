37dacf64cda3cabe22eb79cb1e31bd1e9569b015
Moeen Heydari <moeentk@gmail.com>
2024-07-11 00:54:56 +0200

    110724: Get all the tasks for 'Maryrose Meadows' that were created in september

--- databases/week2/week2-homework-part3.sql
+++ databases/week2/week2-homework-part3.sql
@@ -17,3 +17,12 @@ JOIN user_task ON user.id = user_task.user_id
 JOIN task ON user_task.task_id = task.id
 JOIN status ON task.status_id = status.id
 WHERE user.name = 'Donald Duck' AND status.name = 'Not started';
+
+
+-- Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
+SELECT task.id, task.title, status.name AS status_name
+FROM user
+JOIN user_task ON user.id = user_task.user_id
+JOIN task ON user_task.task_id = task.id
+JOIN status ON task.status_id = status.id
+WHERE user.name = 'Maryrose Meadows' AND MONTH(task.created) = 9;
