44038f42b6ccb8081391072fe43392cdf90aa9ee
Moeen Heydari <moeentk@gmail.com>
2024-07-18 09:57:58 +0200

    180724: SocialMediaDB, user table, post table, comment table was created

---
+++ databases/week3/social_media_exercise.sql
@@ -0,0 +1,36 @@
+-- https://github.com/HackYourFuture-CPH/databases/blob/main/lesson3/social_media_exercise.md
+
+
+CREATE DATABASE SocialMediaDB;
+USE SocialMediaDB;
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    registration_datetime DATETIME NOT NULL
+);
+
+CREATE TABLE post (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    creation_datetime DATETIME NOT NULL,
+    update_datetime DATETIME NOT NULL,
+    author_id INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user(id)
+);
+
+CREATE TABLE comment (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    creation_datetime DATETIME NOT NULL,
+    update_datetime DATETIME NOT NULL,
+    author_id INT NOT NULL,
+    post_id INT NOT NULL,
+    parent_comment_id INT,
+    FOREIGN KEY (author_id) REFERENCES user(id),
+    FOREIGN KEY (post_id) REFERENCES post(id),
+    FOREIGN KEY (parent_comment_id) REFERENCES comment(id)
+);
